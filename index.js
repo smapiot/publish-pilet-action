90e6fd8a72f45ad96d083ac5bae9cf716fca3953
Florian Rappl <florian.rappl@smapiot.com>
2020-01-14 00:28:07 +0100

    Use local version if available

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
-const piral = require('piral-cli');
 const path = require('path');
 const fs = require('fs');
 
@@ -9,27 +8,33 @@ const fullUrl = /^https?:\/\//i;
 const defaultFeed = 'https://feed.piral.io/api/v1/pilet';
 
 async function runAction() {
-  const workspace = process.env.RUNNER_WORKSPACE;
-  const [_, repoName] = process.env.GITHUB_REPOSITORY.split('/');
+  const workspace = process.env.GITHUB_WORKSPACE;
 
-  console.log(`Currently in working directory: ${process.cwd()}`);
   console.log(`Triggered action: ${github.context.action}`);
 
   try {
     const feed = core.getInput('feed');
     const apiKey = core.getInput('api-key');
     const baseDir = core.getInput('base-dir') || '.';
-    const cwd = path.resolve(workspace, repoName, baseDir);
+    const cwd = path.resolve(workspace, baseDir);
     const url = fullUrl.test(feed) ? feed : `${defaultFeed}/${feed}`;
-    const packageJson = path.resolve(cwd, 'package.json');
-    const { version } = require(packageJson);
+    const packageJsonPath = path.resolve(cwd, 'package.json');
+    const piralCliPath = path.resolve(cwd, 'node_modules', 'piral-cli');
+    const { version } = require(packageJsonPath);
 
     if (!fs.existsSync(path.resolve(cwd, 'node_modules'))) {
-      console.warn('Did not find a `node_modules` directory. Trying to resolve dependencies first.');
+      console.warn('Did not find a `node_modules` directory. Trying to resolve dependencies first ...');
       await exec.exec('npm install', undefined, { cwd });
     }
 
-    await piral.apps.publishPilet(cwd, {
+    if (!fs.existsSync(piralCliPath)) {
+      console.warn('Did not find a local `piral-cli` instance. Installing latest ...');
+      await exec.exec('npm install piral-cli --no-save', undefined, { cwd });
+    }
+
+    const piralCli = require(piralCliPath);
+
+    await piralCli.apps.publishPilet(cwd, {
       fresh: true,
       apiKey,
       url,
